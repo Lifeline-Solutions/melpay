aed5a2f3112232c13469ba6b7ac403b122d1c1ae
Nemwel-Boniface <nemwelboniface@outlook.com>
2025-09-14 18:33:11 +0300

    Add needed actions in the users controller to handle user account update and add updated links in the sidebar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,31 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_user, only: [:show, :edit, :update]
   def index
     @users = User.all
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: "Profile updated successfully."
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :phone_number, :email)
+  end
+
+  private
+
+  def set_user
+    @user = current_user
   end
 end
