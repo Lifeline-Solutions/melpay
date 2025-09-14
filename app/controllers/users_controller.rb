a7825b7b19116b754250a8fc3ce0130114c4c751
Nemwel-Boniface <nemwelboniface@outlook.com>
2025-09-14 19:08:07 +0300

    Add pagination to the show all users and update the UI of the users index by listing more info about the user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,18 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update]
   def index
     @users = User.all
+
+    # Pagination setup
+    @per_page = 20
+    @page = (params[:page] || 1).to_i
+
+    @total_count = @users.count
+    @total_pages = (@total_count / @per_page.to_f).ceil
+    @start_count = ((@page - 1) * @per_page) + 1
+    @end_count   = [@page * @per_page, @total_count].min
+
+    # Paginate
+    @users = @users.offset((@page - 1) * @per_page).limit(@per_page)
   end
 
   def show
