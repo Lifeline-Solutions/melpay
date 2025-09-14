f92ca2f2a7d6d3f40c5f3c64f1f9a552eead1310
Nemwel-Boniface <nemwelboniface@outlook.com>
2025-09-14 19:12:01 +0300

    Add more details to the users show and ensure that you are able to see the details of a specific user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:edit, :update]
   def index
     @users = User.all
 
@@ -18,6 +18,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
