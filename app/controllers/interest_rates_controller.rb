fa49323ca4549b162b2b53ae5e9626c4f5d53523
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-10-02 10:59:35 +0300

    Update included:   - Update the interest_rate controller to ensure that we handle both global, sustom rates for clients and management of the interest rates in general   - Update tp the edit interest rates page to have the updated manage interest rate forms/ functionality

--- app/controllers/interest_rates_controller.rb
+++ app/controllers/interest_rates_controller.rb
@@ -1,35 +1,90 @@
 class InterestRatesController < ApplicationController
   before_action :set_system_setting
+  before_action :set_clients, only: [:edit, :manage]
+
+  def index
+    @clients = Client.order(:name)
+    @global_rate = @system_setting.global_interest_rate
+  end
+
+  def manage
+    # This action is used for the management page
+  end
 
   def edit
-    @clients = Client.all
+    render :manage
   end
 
   def update
-    if params[:system_setting].present?
-      # Update global interest rate
-      if @system_setting.update(system_setting_params)
-        flash[:notice] = "Global interest rate updated."
-      else
-        flash[:alert] = "Failed to update global interest rate."
-      end
-    elsif params[:client].present?
-      # Update custom interest rate for a client
-      client = Client.find(params[:client][:id])
-      if client.update(client_params)
-        flash[:notice] = "Custom interest rate updated for #{client.name}."
-      else
-        flash[:alert] = "Failed to update custom interest rate."
-      end
+    if updating_global_rate?
+      update_global_rate
+    elsif updating_client_rate?
+      update_client_rate
+    else
+      redirect_to interest_rates_path, alert: "Nothing to update."
     end
+  end
 
-    redirect_to edit_interest_rates_path
+  # Handle global rate updates
+  def update_global
+    if @system_setting.update(system_setting_params)
+      redirect_to interest_rates_path, notice: "Global interest rate updated."
+    else
+      flash.now[:alert] = "Failed to update global interest rate."
+      @clients = Client.order(:name)
+      render :manage
+    end
+  end
+
+  # Handle custom client rate updates
+  def update_custom
+    if params[:client].present? && params[:client][:id].present?
+      client = Client.find(params[:client][:id])
+      if client.update(custom_interest_rate: params[:client][:custom_interest_rate])
+        redirect_to interest_rates_path, notice: "Custom interest rate updated for #{client.name}."
+      else
+        redirect_to manage_interest_rates_path, alert: "Failed to update custom interest rate for #{client.name}."
+      end
+    else
+      redirect_to manage_interest_rates_path, alert: "Please select a client."
+    end
   end
 
   private
 
   def set_system_setting
     @system_setting = SystemSetting.instance
+  end
+
+  def set_clients
+    @clients = Client.order(:name)
+  end
+
+  def updating_global_rate?
+    params[:system_setting].present?
+  end
+
+  def updating_client_rate?
+    params[:client].present? && params[:client][:id].present?
+  end
+
+  def update_global_rate
+    if @system_setting.update(system_setting_params)
+      redirect_to interest_rates_path, notice: "Global interest rate updated."
+    else
+      flash.now[:alert] = "Failed to update global interest rate."
+      @clients = Client.order(:name)
+      render :manage
+    end
+  end
+
+  def update_client_rate
+    client = Client.find(params[:client][:id])
+    if client.update(client_params)
+      redirect_to interest_rates_path, notice: "Custom interest rate updated for #{client.name}."
+    else
+      redirect_to interest_rates_path, alert: "Failed to update custom interest rate for #{client.name}."
+    end
   end
 
   def system_setting_params
