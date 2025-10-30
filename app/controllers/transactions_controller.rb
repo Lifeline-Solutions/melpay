a78ae557562f6d84b87e25e5658d43b89ead3321
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-10-30 10:17:50 +0300

    Update included:   - Add the transactions index action getting all the transactions   - Update the URL path for the transactions to link to the transactions index path

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,6 +1,10 @@
 class TransactionsController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @transactions = Transaction.all
+  end
+
   def new
     @transaction = Transaction.new
   end
