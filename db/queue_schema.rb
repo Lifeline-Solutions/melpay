c22e3596bc1ddb0607bf85de127fe3998d5ce8da
@ger619 <abolger254@gmail.com>
2025-11-15 11:57:54 +0300

    Remove unnecessary blank line in database configuration

--- db/queue_schema.rb
+++ db/queue_schema.rb
@@ -13,6 +13,7 @@
 ActiveRecord::Schema[8.1].define(version: 2025_11_14_163326) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+  enable_extension "pgcrypto"
 
   create_table "accounts", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
     t.string "account_number"
