67b6d217cd2eb212dd9c75098536dd50986f166b
David Abol Ger <abolger254@gmail.com>
2025-11-16 14:22:31 +0300

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,11 +53,6 @@ Rails.application.configure do
   config.active_job.queue_adapter = :solid_queue
   config.solid_queue.connects_to = { database: { writing: :queue } }
 
-  
-  # Disbale temparily
-  config.solid_cache.enabled = false
-
-
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
