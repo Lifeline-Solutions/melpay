59c7ed4254eb0f8cd886c7808cf6b6ad69ec29b8
Nemwel-Boniface <nemwelnyandoro20@gmail.com>
2025-11-20 21:15:18 +0300

    Update included:   - Remove the Account resource from the ability class   - Add the cancancan ability methods to track the abilities of users to access the sidebar items

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -10,14 +10,12 @@ class Ability
       # Admin can only manage resources within their own client
       can :manage, User, client_id: user.client_id, user_id: user.id
       can :manage, Home, client_id: user.client_id, user_id: user.id
-      can :manage, Account, client_id: user.client_id, user_id: user.id
     elsif user.has_role? :account_manager
       can :read, User, client_id: user.client_id
       can :manage, Home, client_id: user.client_id
     elsif user.has_role? :auditor
       can :read, User, client_id: user.client_id
       can :read, Home, client_id: user.client_id
-      can :read, Account, client_id: user.client_id
     else
       cannot :manage, :all
     end
